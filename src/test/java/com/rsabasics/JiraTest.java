bde9dfae43d00ed852decf195de2c37b4f160ab9
Sreejith <sree.satish131@gmail.com>
2021-06-12 20:09:33 -0400

    POJO class

--- src/test/java/com/rsabasics/JiraTest.java
+++ src/test/java/com/rsabasics/JiraTest.java
@@ -2,6 +2,8 @@ package com.rsabasics;
 
 import static io.restassured.RestAssured.given;
 
+import java.io.File;
+
 import org.apache.http.HttpStatus;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -41,8 +43,20 @@ public class JiraTest {
 		
 	}
 	
-	@Test
+	//@Test
 	public void AddAttachment() {
-		given().header("X-Atlassian-Token", "no-check").filter(session).pathParam("issueID", "10004").when().post("rest/api/2/issue/{issueID}/comment");
+		given().header("X-Atlassian-Token", "no-check").relaxedHTTPSValidation().filter(session).header("Content-Type", "multipart/form-data")
+		.multiPart("file", new File("C:\\Users\\user\\eclipse-workspace\\RestAssuredRSAMain\\RestAssuredRSA\\src\\test\\java\\files\\jiraattachment.jpg"))
+		.pathParam("issueID", "10004").when().post("rest/api/2/issue/{issueID}/comment")
+		.then().statusCode(200);
+	}
+	
+	@Test
+	public void GetIssue() {
+		String response = given().log().all().filter(session).pathParam("issueID", "10004")
+				.queryParam("fields", "comment")
+				.when().get("/rest/api/2/issue/{issueID}")
+				.then().log().all().extract().response().toString();
+		System.out.println(response);
 	}
 }
